c9467a74c6696684a8e964133bdf2c0962be63b0
Ana Luíza <anaferreirahtps@gmail.com>
2026-08-13 15:22:19 -0300

    Add files via upload

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Curso, Disciplina, DisciplinaTecnico, Atividade
 
-admin.site.register(Curso)
+
+class DisciplinaTecnicoInline(admin.TabularInline):
+    model = DisciplinaTecnico.cursos.through
+    extra = 0
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    filter_horizontal = ('disciplinas',)
+    inlines = [DisciplinaTecnicoInline]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        form.base_fields['disciplinas'].queryset = Disciplina.objects.exclude(
+            disciplinatecnico__isnull=False
+        )
+
+        return form
+
 admin.site.register(Disciplina)
 admin.site.register(DisciplinaTecnico)
 admin.site.register(Atividade)
--- sistema/models.py
+++ sistema/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 class Curso(models.Model):
     nome = models.CharField(max_length=100)
-    disciplinas = models.ManyToManyField(
-        'Disciplina',
-        related_name='cursos'
-    )
+    disciplinas = models.ManyToManyField('Disciplina', related_name='cursos', blank=True)
 
     def __str__(self):
         return self.nome
