b32b8f863951030a8ccb4b03e698f5a9f85086e0
Ana Luíza <anaferreirahtps@gmail.com>
2026-08-20 13:40:11 -0300

    Add files via upload

--- sistema/models.py
+++ sistema/models.py
@@ -16,10 +16,7 @@ class Disciplina(models.Model):
 
 
 class DisciplinaTecnico(Disciplina):
-    cursos = models.ManyToManyField(
-        Curso,
-        related_name='disciplinas_tecnicas'
-    )
+    cursos = models.ManyToManyField('Curso', related_name='disciplinas_tecnicas')
 
     def __str__(self):
         return self.nome
@@ -33,18 +30,10 @@ class Atividade(models.Model):
         ('4', 'Quarto'),
     ]
 
-    disciplina = models.ForeignKey(
-        'sistema.Disciplina',
-        on_delete=models.CASCADE,
-        related_name='atividades'
-    )
-
+    disciplina = models.ForeignKey('sistema.Disciplina', on_delete=models.CASCADE, related_name='atividades')
     nome = models.CharField(max_length=100)
     valor = models.FloatField()
-    bimestre = models.CharField(
-        max_length=1,
-        choices=BIMESTRES
-    )
+    bimestre = models.CharField(max_length=1, choices=BIMESTRES)
 
     def __str__(self):
         return self.nome
--- usuario/models.py
+++ usuario/models.py
@@ -2,12 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Aluno(AbstractUser):
-    curso = models.ForeignKey(
-        'sistema.Curso',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    curso = models.ForeignKey('sistema.Curso', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.username
